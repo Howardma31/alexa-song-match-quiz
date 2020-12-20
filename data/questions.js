636b7fd44108cd21a6a39d3c661d2e25e18e92a3
Howard Ma <66407236+Howardma31@users.noreply.github.com>
2020-12-19 23:08:57 -0800

    Create questions.js
    Questions are going to be changed

---
+++ data/questions.js
@@ -0,0 +1,9 @@
+const questionDataCons = [
+                  {Question: 'Which insect represents you the most?',      Selections: ['praying mantis', 'grasshopper', 'scorpian']},
+                  {Question: 'What would you rather do?',                  Selections: ['sky diving', 'bungee jumping', 'sea diving', 'rock climbing']},
+                  {Question: 'Which do you fear the most?',                Selections: ['sky diving', 'bungee jumping', 'sea diving', 'rock climbing']},
+                  {Question: 'Which place do you want to visit the most?', Selections: ['paris', 'las vegas', 'tokyo', 'london']},
+                  {Question: 'Which food do you enjoy the most?',          Selections: ['sushi', 'pizza', 'hamburger', 'hot dog']},
+                  {Question: 'What time of day is your favorite?',         Selections: ['morning', 'afternoon', 'evening', 'night']},
+                  {Question: 'Are you an early bird or night owl?',        Selections: ['early bird', 'night owl']},
+];
