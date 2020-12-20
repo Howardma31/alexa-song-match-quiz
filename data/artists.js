b63d1627538eb1790e6226356fba19cc1abefced
Howard Ma <66407236+Howardma31@users.noreply.github.com>
2020-12-19 23:10:16 -0800

    Create artists.js
    Artists subject to change

---
+++ data/artists.js
@@ -0,0 +1,7 @@
+const data = [
+                {Name: 'ariana grande', Songs: ['God is a woman', 'Positions', 'Rain On Me', '7 rings']},
+                {Name: 'blue',          Songs: ['One Love', 'All Rise', 'Best In Me', 'U Make Me Wanna']},
+                {Name: 'train',         Songs: ['Drops of Jupiter', 'Drive By', 'Hey, Soul Sister', '50 Ways to Say Goodbye']},
+                {Name: 'billy joel',    Songs: ['Uptown Girl', 'Piano Man', 'My Life', 'Vienna']},
+                {Name: 'bon jovi',      Songs: ['Living On A Prayer', 'You Give Love A Bad Name', 'Wanted Dead Or Alive', 'Bed Of Roses']},
+];
