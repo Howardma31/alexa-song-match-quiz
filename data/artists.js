d81a864a3e72376ee3f27f692d4ce0fc1b1fae63
Howard Ma <66407236+Howardma31@users.noreply.github.com>
2020-12-21 16:10:54 -0800

    Update artists.js

--- data/artists.js
+++ data/artists.js
@@ -1,7 +1,7 @@
 const data = [
-                {Name: 'ariana grande', Songs: ['God is a woman', 'Positions', 'Rain On Me', '7 rings']},
-                {Name: 'blue',          Songs: ['One Love', 'All Rise', 'Best In Me', 'U Make Me Wanna']},
-                {Name: 'train',         Songs: ['Drops of Jupiter', 'Drive By', 'Hey, Soul Sister', '50 Ways to Say Goodbye']},
-                {Name: 'billy joel',    Songs: ['Uptown Girl', 'Piano Man', 'My Life', 'Vienna']},
-                {Name: 'bon jovi',      Songs: ['Living On A Prayer', 'You Give Love A Bad Name', 'Wanted Dead Or Alive', 'Bed Of Roses']},
+                {Name: 'ariana grande', Songs: ['God is a woman', 'Positions', 'Rain On Me', '7 rings', 'Santa Tell Me', 'No tears left to cry']},
+                {Name: 'jason mraz',    Songs: [`I'm Yours`, 'Lucky', `I won't give up`, 'Have it all', 'Love someone']},
+                {Name: 'train',         Songs: ['Drops of Jupiter', 'Drive By', 'Hey, Soul Sister', '50 Ways to Say Goodbye', 'Play that song']},
+                {Name: 'pentatonix',    Songs: ['Sing', `Can't sleep love`, 'Cheerleader', 'Problem', 'Daft Punk', 'Rather be']},
+                {Name: 'bon jovi',      Songs: ['Living On A Prayer', 'You Give Love A Bad Name', 'Wanted Dead Or Alive', 'Bed Of Roses', 'Always']},
 ];
